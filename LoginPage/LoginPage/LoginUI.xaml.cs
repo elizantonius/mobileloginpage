ddf5c0d7347ecd65677a85db7462f7761e509419
elizantonius <lonecode1777@gmail.com>
2022-04-29 11:40:07 +0700

    otwbelajar

---
+++ LoginPage/LoginPage/LoginUI.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(usrname.Text=="elz" && paswd.Text=="qwerty9051")
+            {
+                Navigation.PushAsync(new PgUser());
+
+            }
+            else
+            {
+                DisplayAlert("Maaf ","Username atau Password anda salah!", null);
+            }
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
